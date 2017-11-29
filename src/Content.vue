baaf5d20b72a5e607b82c17b10aebc191a9e478c
Aleksandar Ristic <storiaca@gmail.com>
2017-11-29 13:52:27 +0100

    Switching between views

--- src/Content.vue
+++ src/Content.vue
@@ -1,9 +1,55 @@
 <template>
-  <h1>Content</h1>
+  <aside class="lg-side">
+    <div class="inbox-head">
+      <h3>{{ currentView.title }}</h3>
+    </div>
+
+    <keep-alive>
+      <component :is="currentView.tag"></component>
+    </keep-alive>
+  </aside>
 </template>
 
 <script>
-export default {
+  import Inbox from './Inbox.vue';
+  import Trash from './Trash.vue';
+  import Sent from './Sent.vue';
+  import Important from './Important.vue';
+  import ViewMessage from './ViewMessage.vue';
+  import { eventBus } from './main';
   
-}
+  export default {
+    created() {
+      eventBus.$on('changeView', (data) => {
+        let temp = [{
+          tag: data.tag,
+          title: data.title
+        }];
+
+        this.history = temp.concat(this.history.splice(0));
+      });
+    },
+    data() {
+      return {
+        history: [
+          {
+            tag: 'app-inbox',
+            title: 'Inbox'
+          }
+        ]
+      };
+    },
+    computed: {
+      currentView() {
+        return this.history[0];
+      }
+    },
+    components: {
+      appInbox: Inbox,
+      appTrash: Trash,
+      appSent: Sent,
+      appImportant: Important,
+      appViewMessage: ViewMessage
+    }
+  }
 </script>
--- src/Sidebar.vue
+++ src/Sidebar.vue
@@ -10,23 +10,23 @@
     </div>
 
     <ul class="inbox-nav">
-      <li class="active">
-        <a href="#">
+      <li :class="{ active: activeView == 'app-inbox'}">
+        <a href="#" @click.prevent="navigate('app-inbox', 'Inbox')">
           <i class="fa fa-inbox"></i>Inbox <span class="label label-danger pull-right">?</span>
         </a>
       </li>
-      <li>
-        <a href="#">
+      <li :class="{ active: activeView == 'app-sent'}">
+        <a href="#" @click.prevent="navigate('app-sent', 'Sent')">
           <i class="fa fa-envelope-o"></i>Sent <span class="label label-default pull-right">?</span>
         </a>
       </li>
-      <li>
-        <a href="#">
+      <li :class="{ active: activeView == 'app-important'}">
+        <a href="#" @click.prevent="navigate('app-important', 'Important')">
           <i class="fa fa-bookmark-o"></i>Important <span class="label label-warning pull-right">?</span>
         </a>
       </li>
-      <li>
-        <a href="#">
+      <li :class="{ active: activeView == 'app-trash'}">
+        <a href="#" @click.prevent="navigate('app-trash', 'Trash')">
           <i class="fa fa-trash-o"></i>Trash <span class="label label-default pull-right">?</span>
         </a>
       </li>
@@ -35,8 +35,27 @@
 </template>
 
 <script>
-export default {
-  
-}
+  import { eventBus } from './main';
+
+  export default {
+    created() {
+      eventBus.$on('changeView', (data) => {
+        this.activeView = data.tag;
+      });
+    },
+    data() {
+      return {
+        activeView: 'app-inbox'
+      };
+    },
+    methods: {
+      navigate(newView, title) {
+        eventBus.$emit('changeView', {
+          tag: newView,
+          title: title
+        })
+      }
+    }
+  }
 </script>
 
