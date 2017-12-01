0157d571d33c8b73a43f32a59f572346df475f34
Aleksandar Ristic <storiaca@gmail.com>
2017-12-01 13:29:07 +0100

    Refresh inbox

--- src/Inbox.vue
+++ src/Inbox.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="inbox-body">
+    <div class="mail-option">
+      <div class="btn-group">
+          <a href="#" class="btn" @click.prevent="refresh">
+            <i class="fa fa-refresh"></i>&nbsp; Refresh
+          </a>
+      </div>
+    </div>
+    
     <app-messages :messages="incomingMessages"></app-messages>
   </div>
 </template>
 
 <script>
   import Messages from './Messages.vue';
+  import { eventBus } from './main';
 
   export default {
     props: {
       data: {
         type: Object,
         required: true
+      }
+    },
+    methods: {
+      refresh() {
+        eventBus.$emit('refreshMessages')
       }
     },
     computed: {
